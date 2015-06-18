375d37439a8db32ab6a50732eac08ed21533e782
Nicht Jens <nichtjens@gmail.com>
2015-06-18 20:57:37 +0200

    moved actual module code from __init__.py to an eponymous file so that it can be used standalone or in the folder structure

--- ctrl_c/__init__.py
+++ ctrl_c/__init__.py
@@ -1,9 +1,3 @@
-import sys, signal
 
-# http://tldp.org/LDP/abs/html/exitcodes.html
-
-def ctrl_c(ret_value=128 + signal.SIGINT):
-    signal.signal(signal.SIGINT, lambda x,y: sys.exit(ret_value))
-
-ctrl_c()
+from ctrl_c import ctrl_c
 

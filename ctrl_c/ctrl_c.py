5222f398ef85d3e874e1a692b019d3d3de465d46
Auf keinen Fall Jens <NichtJens@users.noreply.github.com>
2015-08-23 11:31:07 +0200

    small cleanup

--- ctrl_c/ctrl_c.py
+++ ctrl_c/ctrl_c.py
@@ -1,9 +1,10 @@
 import sys, signal
 
 # http://tldp.org/LDP/abs/html/exitcodes.html
+RET_VALUE = 128 + signal.SIGINT
 
-def ctrl_c(ret_value=128 + signal.SIGINT):
-    signal.signal(signal.SIGINT, lambda x,y: sys.exit(ret_value))
+def ctrl_c(ret_value=RET_VALUE):
+    signal.signal(signal.SIGINT, lambda x, y: sys.exit(ret_value))
 
 ctrl_c()
 

13ddf3da1bb9a03dbeeb32088bdada86f8456d1d
Auf keinen Fall Jens <NichtJens@users.noreply.github.com>
2015-08-23 11:34:19 +0200

    Print a newline after ctrl-c was signalled and before exiting

--- ctrl_c/ctrl_c.py
+++ ctrl_c/ctrl_c.py
@@ -4,7 +4,11 @@ import sys, signal
 RET_VALUE = 128 + signal.SIGINT
 
 def ctrl_c(ret_value=RET_VALUE):
-    signal.signal(signal.SIGINT, lambda x, y: sys.exit(ret_value))
+    signal.signal(signal.SIGINT, lambda x, y: exit(ret_value))
+
+def exit(ret_value):
+    print
+    sys.exit(ret_value)
 
 ctrl_c()
 
